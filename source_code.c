6aa6d56669a14ea18f30c186991c776ae1bc1c98
MOHAMED FARHUN <86124759+MohamedFarhun@users.noreply.github.com>
2022-09-18 19:42:03 +0530

    Create source_code.c

---
+++ source_code.c
@@ -0,0 +1,67 @@
+ESP8266 Code
+#include “ThinkSpeak.h
+#include <ESP8266WiFi.h>
+char networkname[] = “”; // your network name
+char passcode[] = “”; // your passcode
+WiFiClient client;
+unsigned long tsChannelID = ; // ThingSpeak Channel ID
+const char * tsWriteAPIKey = “”; //ThingSpeak Write API Key
+String airQuaility = “”;
+const int fieldOne = 1;
+void setup()
+{
+Serial.begin(115200);
+WiFi.mode(WIFI_STA);
+ThingSpeak.begin(client);
+thingSpeak();
+}
+void loop()
+{
+thingSpeak();
+if (Serial.available() > 0)
+{
+while (Serial.available() > 0)
+{
+int inChar = Serial.read();
+airQuaility += (char)inChar;
+}
+}
+pushData();
+}
+void thingSpeak()
+{
+if (WiFi.status() != WL_CONNECTED)
+{
+while (WiFi.status() != WL_CONNECTED)
+{
+WiFi.begin(networkname, passcode);
+delay(5000);
+}
+}
+}
+void pushData()
+{
+int getData = ThingSpeak.writeField(tsChannelID, fieldOne, airQuaility, tsWriteAPIKey);
+if (getData != 200)
+{
+delay(15000);
+pushData();
+}
+airQuaility = “”;
+}
+Final Code
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(5, 6);
+int aqValue;
+void setup()
+{
+mySerial.begin(115200);
+Serial.begin(115200);
+}
+void loop()
+{
+aqValue = analogRead(A0);
+Serial.println(aqValue);
+mySerial.println(aqValue);
+delay(300000);
+}
